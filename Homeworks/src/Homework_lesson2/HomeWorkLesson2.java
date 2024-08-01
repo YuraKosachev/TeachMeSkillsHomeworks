e60b65e528359b1ccb47b5878e52bd46880f61d1
Косачёв Юрий <y.kosachou@sam-solutions.com>
2024-08-02 00:26:44 +0300

    added new homework's task 4

--- Homeworks/src/Homework_lesson2/HomeWorkLesson2.java
+++ Homeworks/src/Homework_lesson2/HomeWorkLesson2.java
@@ -5,7 +5,9 @@ public class HomeWorkLesson2 {
         double task1Result = getTask1Example(2, 4);
         int task2Result = getTwoDigitSum(26);
         int task3Result = getThreeDigitSum(999);
+        long task4Result = getRound(2.2);
         System.out.printf("Задача1: %.2f\nЗадача 2: %d\nЗадача 3: %d\n", task1Result, task2Result, task3Result);
+        System.out.printf("Задача 4: %d\n", task4Result);
     }
 
     //Task 1
@@ -21,6 +23,11 @@ public class HomeWorkLesson2 {
     //Task 3
     static int getThreeDigitSum(int src){
         return src / 100 + getTwoDigitSum(src % 100);
+    }
+
+    //Task 4
+    static long getRound(double src){
+        return Math.round(src);
     }
 
 }
