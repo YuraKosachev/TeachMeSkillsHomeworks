235e06cb2c08108bd956123b3a8ec557b1b58903
Косачёв Юрий <y.kosachou@sam-solutions.com>
2024-08-22 13:01:07 +0300

    made Helper class static

--- Homeworks/src/Homework_lesson7/Helpers.java
+++ Homeworks/src/Homework_lesson7/Helpers.java
@@ -2,7 +2,7 @@ package Homework_lesson7;
 
 public final class Helpers {
 
-    public static final Doctor getDoctorByClass(Doctor[] doctors, Class doctorClass){
+    public static Doctor getDoctorByClass(Doctor[] doctors, Class doctorClass){
         for(Doctor doctor : doctors ){
             if(doctor.getClass() == doctorClass)
                 return doctor;
