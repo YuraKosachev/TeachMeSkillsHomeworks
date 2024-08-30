d7299b071418dfde7c9778000b6a4c882089a50b
Косачёв Юрий <y.kosachou@sam-solutions.com>
2024-08-30 15:02:31 +0300

    change deepCopy method

--- Homeworks/src/Homework_lesson10/User.java
+++ Homeworks/src/Homework_lesson10/User.java
@@ -1,5 +1,8 @@
 package Homework_lesson10;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -67,10 +70,26 @@ public class User implements Cloneable {
     public User deepClone() throws CloneNotSupportedException {
         try {
             User userClone = this.clone();
-            userClone.setLocation(this.location.clone());
+
+            Field[] fields = userClone.getClass().getDeclaredFields();
+
+            for(Field field: fields){
+                field.setAccessible(true);
+                Object obj = field.get(userClone);
+                if(obj instanceof  Cloneable){
+                    Method method = obj.getClass().getMethod("clone");
+                    if(method == null)
+                        continue;
+                    field.set(userClone, method.invoke(obj));
+                }
+            }
             return userClone;
-        } catch (CloneNotSupportedException ex) {
+        } catch (CloneNotSupportedException | IllegalAccessException ex) {
             throw new CloneNotSupportedException(ex.getMessage());
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
         }
     }
 }
